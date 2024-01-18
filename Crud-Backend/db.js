ecbcb09bdd9f76a041928d1a29efb2e6169b0830
Manish Goud <152627301+ManishGoud7828@users.noreply.github.com>
2024-01-18 11:57:42 +0530

    Update db.js

--- Crud-Backend/db.js
+++ Crud-Backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
 
 
-mongoose.connect('mongodb+srv://AshuPanda:b959bL3lPfxV8VlE@cluster0.q42yghj.mongodb.net/crud-app?retryWrites=true&w=majority')
+mongoose.connect('your database put')
   .then((response) => {
     console.log("DataBase is Connected Successfully--!");
   })
